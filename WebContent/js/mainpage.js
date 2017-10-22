67c05c2849c12b2b3156375028c7745166bc58ae
CDXQ <CDXQ@DESKTOP-LOR06UO>
2017-10-22 14:26:36 +0800

    1)完善刪除編輯

--- WebContent/js/mainpage.js
+++ WebContent/js/mainpage.js
@@ -1,35 +1,49 @@
 var gridId = "rowed";
+var id = '';
+var wuliaoguanxiObj = {
+		1170927:'一类物料',
+		2170927:'二类物料',
+		3170927:'三类物料',
+		4170927:'四类物料',
+		5170927:'五类物料',
+		6170927:'六类物料',
+		7170927:'七类物料'
+}
 $(function(){
 	//页面加载完成之后执行
-	pageInit();
-	flexingGrid(gridId);
-	var id = GetQueryString("id");
+	id = GetQueryString("id");
 	showWhichSidebar(id);
+	FaDianLiangLuRu();
+	flexingGrid(gridId);
 });
-function pageInit(){
+function FaDianLiangLuRu(){
 	//创建jqGrid组件
 	var lastsel3;
+	$('#main_content').empty().html('<table id="rowed"></table>'+'<div id="pager" ></div>');
 	jQuery("#rowed").jqGrid(
 			{
-				url : 'data/JSONData.json',//组件创建完成之后请求数据的url
+				url : 'data/fadianliangluru.json',//组件创建完成之后请求数据的url
 				styleUI : 'Bootstrap',
 				datatype : "json",//请求数据返回的类型。可选json,xml,txt
 				colModel : [ //jqGrid每一列的配置信息。包括名字，索引，宽度,对齐方式.....
-				             {label:'业务单元（公司）', name : 'yewudanyuan',width : 160,editable: true,edittype : "select",editoptions : {value : "0:A公司;1:B公司;2:C公司"},align:'center',}, 
-				             {label:'单据编号', name : 'danjubianhao',width : 90,editable: true,align : "center"}, 
-				             {label:'生产部门', name : 'shengchanbianhao',width : 100,editable: true,edittype : "select",editoptions : {value : "0:A部门;1:B部门;2:C部门"},align:'center'}, 
-				             {label:'日期', name : 'sdate',width : 100,editable : true,sorttype : "date",align : "center"}, 
+				             {label:'业务单元（公司）', name : 'yewudanyuan',width : 160,align:'center',}, 
+				             {label:'单据编号', name : 'danjubianhao',width : 90,align : "center"}, 
+				             {label:'生产部门', name : 'shengchanbumen',width : 100,align:'center'}, 
+				             {label:'日期', name : 'riqi',width : 100,sorttype : "date",align : "center"}, 
 				             {label:'录入人', name : 'lururen',width : 80,align:'center'}, 
-				             {label:'物料编码', name : 'wuliaobianma',width : 80,align : "center",editable : true,editoptions : {size : 25}}, 
-				             {label:'物料名称', name : 'wuliaomingcheng',width : 150,editoptions : {readonly : true,size : 10}},
-				             {label:'发电量', name : 'fadianliang',width : 80,editable : true,editoptions : {size : 25},align : "right"} ,
-				             {label:'单价（元）', name : 'danjia',width : 80,editable : true,editoptions : {size : 25},align : "right"} ,
-				             {label:'金额（元）', name : 'jine',width : 80,editable : true,editoptions : {size : 25},align : "right"} 
+				             {label:'物料编码', name : 'wuliaobianma',width : 80,align : "center",}, 
+				             {label:'物料名称', name : 'wuliaomingcheng',width : 150,align : "center"},
+				             {label:'发电量', name : 'fadianliang',width : 80,align : "right"} ,
+				             {label:'单价', name : 'danjia',width : 80,align : "right",formatter:'currency'} ,
+				             {label:'金额', name : 'jine',width : 80,align : "right",formatter:'currency'},
+				             {label:'公司标识', name : 'gongsibiaoshi',width : 80,align : "right",hidden: "true"} ,
+				             {label:'部门标识', name : 'bumenbiaoshi',width : 80,align : "right",hidden: "true"} ,
+				             {label:'录入人标识', name : 'lururenbiaoshi',width : 80,align : "right",hidden: "true"} ,
+				             {label:'物料标识', name : 'wuliaobiaoshi',width : 80,align : "right",hidden: "true"} ,
 				           ],
 				onSelectRow : function(id) {
 		          if (id && id !== lastsel3) {
 		            jQuery('#rowed').jqGrid('restoreRow', lastsel3);
-		            jQuery('#rowed').jqGrid('editRow', id, true, pickdates);
 		            lastsel3 = id;
 		          }
 		        },
@@ -71,6 +85,146 @@ function pageInit(){
 		} 
 	});
 	$('#gbox_rowed').addClass('paddingoverflow');
+	flexingGrid(gridId);
+}
+function JieSuanDianLiangLuRu(){
+	//创建jqGrid组件
+	var lastsel3;
+	$('#main_content').html('<table id="rowed"></table>'+'<div id="pager" ></div>');
+	jQuery("#rowed").jqGrid(
+			{   
+				url : 'data/JSONData.json',//组件创建完成之后请求数据的url
+				styleUI : 'Bootstrap',
+				datatype : "json",//请求数据返回的类型。可选json,xml,txt
+				colModel : [ //jqGrid每一列的配置信息。包括名字，索引，宽度,对齐方式.....
+				             {label:'业务单元（公司）', name : 'yewudanyuan',width : 160,editable: true,edittype : "select",editoptions : {value : "0:A公司;1:B公司;2:C公司"},align:'center',}, 
+				             {label:'单据编号', name : 'danjubianhao',width : 90,editable: true,align : "center"}, 
+				             {label:'部门', name : 'shengchanbumen',width : 100,editable: true,edittype : "select",editoptions : {value : "0:A部门;1:B部门;2:C部门"},align:'center'}, 
+				             {label:'日期', name : 'sdate',width : 100,editable : true,sorttype : "date",align : "center"}, 
+				             {label:'客户编码', name : 'lururen',width : 80,align:'center'}, 
+				             {label:'客户名称', name : 'lururen',width : 80,align:'center'}, 
+				             {label:'物料编码', name : 'wuliaobianma',width : 80,align : "center",editable : true,editoptions : {size : 25}}, 
+				             {label:'物料名称', name : 'wuliaomingcheng',width : 150,editoptions : {readonly : true,size : 10}},
+				             {label:'结算电量', name : 'fadianliang',width : 80,editable : true,editoptions : {size : 25},align : "right"} ,
+				             {label:'标杆单价', name : 'danjia',width : 80,editable : true,editoptions : {size : 25},align : "right",formatter:'currency'} ,
+				             {label:'标杆金额', name : 'danjia',width : 80,editable : true,editoptions : {size : 25},align : "right",formatter:'currency'} ,
+				             {label:'补贴单价', name : 'danjia',width : 80,editable : true,editoptions : {size : 25},align : "right",formatter:'currency'} ,
+				             {label:'补贴金额', name : 'danjia',width : 80,editable : true,editoptions : {size : 25},align : "right",formatter:'currency'} ,
+				           ],
+				onSelectRow : function(id) {
+		          if (id && id !== lastsel3) {
+		            jQuery('#rowed').jqGrid('restoreRow', lastsel3);
+		            jQuery('#rowed').jqGrid('editRow', id, true, pickdates);
+		            lastsel3 = id;
+		          }
+		        },
+				rowNum : 1000,//一页显示多少条
+				pgbuttons : false,//是否显示翻页按钮
+				pgtext	: null,
+				hidegrid : false,
+				pager : '#pager',//表格页脚的占位符(一般是div)的id
+				sortname : 'id',//初始化的时候排序的字段
+				sortorder : "desc",//排序方式,可选desc,asc
+				mtype : "post",//向后台请求数据的ajax的类型。可选post,get
+				viewrecords : true,
+				caption : "结算电量录入单据",//表格的标题名字
+				width: 'auto',
+				height: '100%',
+				autowidth: true,
+				forceFit: false,
+			});
+	jQuery("#rowed").jqGrid("navGrid", "#pager",
+			{edit: false, add: false, del: false, refresh: false, search:false});
+	
+	jQuery("#rowed").jqGrid('navButtonAdd', "#pager", {caption:'新增',title:'新增项。', 
+		id: gridId + '_newFaDianLiangDanJu', buttonicon :'fa fa-plus',
+		onClickButton:function(){
+			manipulateGongCheng('新增');
+			
+		} 
+	});
+	jQuery("#rowed").jqGrid('navButtonAdd', "#pager", {caption:'修改',title:'修改选中项。', 
+		id: gridId + '_editFaDianLiangDanJu', buttonicon :'fa fa-pencil-square-o',
+		onClickButton:function(){
+			manipulateGongCheng('编辑');
+		} 
+	});
+	jQuery("#rowed").jqGrid('navButtonAdd', "#pager", {caption:'删除',title:'删除选中项。', 
+		id: gridId + '_delFaDianLiangDanJu', buttonicon :'fa fa-trash',
+		onClickButton:function(){
+			manipulateGongCheng('删除');
+		} 
+	});
+	$('#gbox_rowed').addClass('paddingoverflow');
+	flexingGrid(gridId);
+}
+
+function DianLiangChanXiaoCunLuRu(){
+	//创建jqGrid组件
+	var lastsel3;
+	$('#main_content').empty().html('<table id="rowed"></table>'+'<div id="pager" ></div>');
+	jQuery("#rowed").jqGrid(
+			{
+				url : 'data/JSONData.json',//组件创建完成之后请求数据的url
+				styleUI : 'Bootstrap',
+				datatype : "json",//请求数据返回的类型。可选json,xml,txt
+				colModel : [ //jqGrid每一列的配置信息。包括名字，索引，宽度,对齐方式.....
+				             {label:'业务单元（公司）', name : 'yewudanyuan',width : 160,align:'center',}, 
+				             {label:'会计期间', name : 'danjubianhao',width : 90,editable: true,align : "center"}, 
+				             {label:'编码', name : 'lururen',width : 80,align:'center'}, 
+				             {label:'名称', name : 'lururen',width : 80,align:'center'}, 
+				             {label:'生产总量', name : 'wuliaobianma',width : 80,align : "center",editable : true,editoptions : {size : 25}}, 
+				             {label:'结算总量', name : 'wuliaomingcheng',width : 150,editoptions : {readonly : true,size : 10}},
+				             {label:'结存电量', name : 'fadianliang',width : 80,editable : true,editoptions : {size : 25},align : "right"} ,
+				             {label:'结存金额', name : 'fadianliang',width : 80,editable : true,editoptions : {size : 25},align : "right",formatter:'currency'} ,
+				             {label:'公司标识', name : 'gongshibiaoshi',width : 80,align : "right"} ,
+				           ],
+				onSelectRow : function(id) {
+		          if (id && id !== lastsel3) {
+		            jQuery('#rowed').jqGrid('restoreRow', lastsel3);
+		            jQuery('#rowed').jqGrid('editRow', id, true, pickdates);
+		            lastsel3 = id;
+		          }
+		        },
+				rowNum : 1000,//一页显示多少条
+				pgbuttons : false,//是否显示翻页按钮
+				pgtext	: null,
+				hidegrid : false,
+				pager : '#pager',//表格页脚的占位符(一般是div)的id
+				sortname : 'id',//初始化的时候排序的字段
+				sortorder : "desc",//排序方式,可选desc,asc
+				mtype : "post",//向后台请求数据的ajax的类型。可选post,get
+				viewrecords : true,
+				caption : "电量产销存报表",//表格的标题名字
+				width: 'auto',
+				height: '100%',
+				autowidth: true,
+				forceFit: false,
+			});
+	jQuery("#rowed").jqGrid("navGrid", "#pager",
+			{edit: false, add: false, del: false, refresh: false, search:false});
+	
+	jQuery("#rowed").jqGrid('navButtonAdd', "#pager", {caption:'新增',title:'新增项。', 
+		id: gridId + '_newFaDianLiangDanJu', buttonicon :'fa fa-plus',
+		onClickButton:function(){
+			manipulateGongCheng('新增');
+			
+		} 
+	});
+	jQuery("#rowed").jqGrid('navButtonAdd', "#pager", {caption:'修改',title:'修改选中项。', 
+		id: gridId + '_editFaDianLiangDanJu', buttonicon :'fa fa-pencil-square-o',
+		onClickButton:function(){
+			manipulateGongCheng('编辑');
+		} 
+	});
+	jQuery("#rowed").jqGrid('navButtonAdd', "#pager", {caption:'删除',title:'删除选中项。', 
+		id: gridId + '_delFaDianLiangDanJu', buttonicon :'fa fa-trash',
+		onClickButton:function(){
+			manipulateGongCheng('删除');
+		} 
+	});
+	$('#gbox_rowed').addClass('paddingoverflow');
+	flexingGrid(gridId);
 }
 
 function flexingGrid(gridId) {
@@ -91,35 +245,88 @@ function manipulateGongCheng(strAction){
 		$.get('templates/fadianliangluru.html', function(data){
 			newProjectDialog.innerHTML = data;
 			$(newProjectDialog).dialog({
-				title: '！',
+				title: '新增单据',
+				width: 710,
 				position: { my: "center", at: "center", of: window },
 				buttons: {
 			 		"确定" :	function() {
-
+			 			addRowData(newProjectDialog);
+			 			closeDialog(this);
 			 		},
 					"退出" : function() {
 						closeDialog(this);
 					}
 				}
 			}); 
+			$(".date-picker-elem").datepicker();
 			
 			$(newProjectDialog).dialog({
 				position: { my: "center", at: "center", of: window },
 			});
 		},'text');
 		newProjectDialog.className='dialog-body';
-		$(newProjectDialog).dialog({
-			title: '！',
+	}
+	function editProject(){
+		var gridId = "#rowed";
+		var selectedId = $(gridId).jqGrid('getGridParam', 'selrow');
+		if (!selectedId) {
+			alert('请先选中您要需要修改的明细行！', '没有选中要修改的记录', 'wrong');
+			return;
+		}
+		var theData = $(gridId).jqGrid('getRowData', selectedId);
+		var editDialog = document.createElement('div');
+		$.get('templates/fadianliangluru.html', function(data){
+			editDialog.innerHTML = data;
+			fillElementsWithData(theData,editDialog);
+			$(editDialog).dialog({
+				title: '修改单据',
+				width: 710,
+				position: { my: "center", at: "center", of: window },
+				buttons: {
+					"确定" :	function() {
+						editRowData(editDialog,selectedId);
+						closeDialog(this);
+					},
+					"退出" : function() {
+						closeDialog(this);
+					}
+				}
+			}); 
+			$(".date-picker-elem").datepicker();
+			
+			$(editDialog).dialog({
+				position: { my: "center", at: "center", of: window },
+			});
+		},'text');
+		editDialog.className='dialog-body';
+	}
+	function deleteProject(){
+		var gridId = "#rowed";
+		var selectedId = $(gridId).jqGrid('getGridParam', 'selrow');
+		if (!selectedId) {
+			alert('请先选中您要需要修改的明细行！', '没有选中要修改的记录', 'wrong');
+			return;
+		}
+		var deleteDialog = document.createElement('div');
+		deleteDialog.innerHtml =  "<div style='height:20px,width:30px'>你在执行删除操作，如果是误操作点击”取消“按钮，点击”确认“完成删除</div>";
+		$(deleteDialog).dialog({
+			title: '删除单据',
+			width: 710,
 			position: { my: "center", at: "center", of: window },
 			buttons: {
-		 		"确定" :	function() {
-
-		 		},
-				"退出" : function() {
+				"确定" :	function() {
+					deleteRowData(deleteDialog,selectedId);
+					closeDialog(this);
+				},
+				"取消" : function() {
 					closeDialog(this);
 				}
 			}
 		}); 
+		$(deleteDialog).dialog({
+			position: { my: "center", at: "center", of: window },
+		});
+		deleteDialog.className='dialog-body';
 	}
 }
 function closeDialog(theDialogElem) {
@@ -133,20 +340,55 @@ function closeDialog(theDialogElem) {
 	}
 };
 
-//用于控制sidebar的显示
+//用于控制sidebar的显示内容
 function showWhichSidebar(id){
 	var htmlStr = "";
-	if(id=="admin"){
-		htmlStr +="<li><a href=''><span><i class='img-icon fa fa-book'></i>发电量录入单据</span></a></li>"
-		htmlStr +="<li><a href=''><span><i class='img-icon fa fa-book'></i></i>结算电量单据</span></a></li>"
-		htmlStr +="<li><a href=''><span><i class='img-icon fa fa-book'></i></i>结存电量处理</span></a></li>" 
-		$("#baobiao_sidebar").html(htmlStr);	
+	if(id=="yonghu"){
+		htmlStr +="<li id='fadianliang_col' class='sidebar-col'><a href='javascript: void sidebarChecked(\"发电\");'><span><i class='img-icon fa fa-book'></i>发电量录入单据</span></a></li>"
+		htmlStr +="<li id='jiesuandianliang_col' class='sidebar-col'><a href='javascript: void sidebarChecked(\"结算\");'><span><i class='img-icon fa fa-book'></i></i>结算电量单据</span></a></li>"
+		htmlStr +="<li id='jiecundianliang_col' class='sidebar-col'><a href='javascript: void sidebarChecked(\"结存\");'><span><i class='img-icon fa fa-book'></i></i>结存电量处理</span></a></li>" 
+		$("#baobiao_sidebar").html(htmlStr);
+		$('#fadianliang_col').addClass('active');
 	}else{
-		htmlStr +="<li><a href=''><span><i class='img-icon fa fa-book'></i>电站经营汇总报表</span></a></li>"
-		htmlStr +="<li><a href=''><span><i class='img-icon fa fa-book'></i></i>集团经营汇总报表</span></a></li>"
+		htmlStr +="<li id='dianzhanzonghui_col' class='sidebar-col'><a href='javascript: void sidebarChecked(\"电站总汇\");'><span><i class='img-icon fa fa-book'></i>电站经营汇总报表</span></a></li>"
+		htmlStr +="<li id='jituanzonghui_col' class='sidebar-col'><a href='javascript: void sidebarChecked(\"集团总汇\");'><span><i class='img-icon fa fa-book'></i></i>集团经营汇总报表</span></a></li>"
 		$("#baobiao_sidebar").html(htmlStr);	
 	}
 }
 
+function sidebarChecked(strAction){
+	if(strAction == '发电'){
+		$('.sidebar-col').removeClass('active');
+		$('#fadianliang_col').addClass('active');
+		FaDianLiangLuRu();
+	}else if(strAction == '结算'){
+		$('.sidebar-col').removeClass('active');
+		$('#jiesuandianliang_col').addClass('active');
+		JieSuanDianLiangLuRu();
+	}else if(strAction == '结存'){
+		$('.sidebar-col').removeClass('active');
+		$('#jiesuandianliang_col').addClass('active');
+		DianLiangChanXiaoCunLuRu();
+	}else if(strAction == '电站总汇'){
+		
+	}else if(strAction == '集团总汇'){
+		
+	}
+}
 
+/*新增行*/
+function addRowData(newProjectDialog){
+    var willGoingData = serializeObject(newProjectDialog);
+    jQuery("#rowed").jqGrid("addRowData",11,willGoingData,"last");
+}
 
+/*编辑行*/
+function editRowData(editDialog,selectedId) {
+	var willGoingData = serializeObject(editDialog);
+	jQuery("#rowed").jqGrid("setRowData",selectedId,willGoingData,"false");
+}
+
+/*删除行*/
+function deleteRowData(deleteDialog,selectedId){
+	jQuery("#rowed").jqGrid("delRowData",selectedId);
+}
--- WebContent/js/utils.js
+++ WebContent/js/utils.js
@@ -3,3 +3,118 @@ function GetQueryString(name) {
 	var r = window.location.search.substr(1).match(reg); 
 	if (r!=null) return (r[2]); return null; 
 } 
+
+function iniContextObject(objContext, strParentSelector) {
+	if (objContext == undefined) {
+		return $('body');
+	} else {
+		if (strParentSelector) {
+			if ($(objContext).is(strParentSelector)) {
+				return $(objContext);
+			}
+			var parentObj = $(objContext).closest(strParentSelector);
+			if (parentObj.length == 0) {
+				return $('body');
+			} else {
+				return parentObj;
+			}
+		} else {
+			return $(objContext);
+		}
+	}
+}
+
+function fillElementsWithData(theData,objContext){
+	if(!theData) return;
+	objContext = iniContextObject(objContext);
+	var inputElems = [];
+	inputElems = $('input,select,textarea,.content-label',objContext).not('[type="checkbox"]').not('.ui-pg-input').not(
+	'.not-fillin').not('.ui-datepicker-year').not(
+	'.ui-datepicker-month');
+	for(var i = 0;i<inputElems.length;i++){
+		if(theData.hasOwnProperty(inputElems[i].name)){
+			if(inputElems[i].type === 'select-one'){
+				if($(inputElems[i]).children('option').length > 0) {
+					inputElems[i].value = theData[inputElems[i].name]
+				}
+			} else {
+				inputElems[i].value = theData[inputElems[i].name];
+			}
+		}
+	}
+}
+
+/*
+ * 根据select的选择带出后面输入框的value,前提： 无 输入： 参数1：Object, select的值与只对应的值;
+ * 参数2：domElem,select选中项对应的DOM
+ * 参数3：domElem,显示区域对应的DOM
+ * 出口： 返回调用本函数的页面 返回： 无 改进： 暂无
+ */
+function showDefaultVal(selectName,inputName,showName,dataObj){
+	   var optionSelected = $("select[name='"+selectName+"']").find("option:selected");
+       var valueSelected  = optionSelected.text();
+       if(dataObj){
+    	   $("input[name="+showName+"]").val(dataObj[valueSelected]);
+       }
+       $("input[name="+inputName+"]").val(valueSelected);
+       
+}
+
+/*
+ * 将页面element的name和value转换为name:value格式的js Object
+ * jQuery的ajax函数支持以上形式的object作为post参数， 也支持
+ * {name=name1,value=value1;name=name2,value=value2;....}形式的post参数 还支持
+ * {name:value}[]数组形式的post参数。
+ * 但是，在jqgrid控件中，如果要实现分页显示，则只能使用第一种形式的参数格式。本函数就是因为这个目的而存在的。
+ * 
+ * 前提： 无 输入: String或jQuery对象, 需要转换的元素所在的父容器或父容器选择器字符，用于限定转换的元素选择范围; 出口： 维持在原页面
+ * 返回： js Object= {name1:value1,name2:value2,.....} 改进： 暂无
+ * 
+ * @author Harry
+ * 
+ * @version 1.0 2013-12-24
+ * 
+ * 1.1 版本简述 增加了对输入单元是否有value的判断，允许函数返回空对象
+ * 
+ * @author Harry
+ * @version 1.1 2014-12-17
+ * 
+ * 1.2 版本简述 增加了采集含数据信息的LABEL元素的值的功能。 规则：字段名应作为LABEL的id，且LABEL元素应属于with-data类
+ * 
+ * @author Harry
+ * @version 1.2 2015-10-09
+ */
+function serializeObject(objContext) {
+	objContext = iniContextObject(objContext);
+	var inputElems = $('input,select,textarea', objContext).not('.ui-pg-input')
+			.not('.not-serialize');
+	var returnObj = {};
+	// 对空字符，js在chrome和ie环境下的处理方式不同。
+	// chrome下，obj的某个属性值为空，则这个属性在控制台不被打印，
+	// 而ie下，空值的属性仍然会在控制台打印出来，
+	// 但传递到后面程序时，仿佛两个平台是一样的，都没有空字符的属性。
+	for (var i = 0; i < inputElems.length; i++) {
+		if (inputElems[i].type == 'checkbox') {
+			returnObj[inputElems[i].name] = inputElems[i].checked;
+		} else {
+			if (inputElems[i].value) {
+				if ($(inputElems[i]).hasClass('percent')) {
+					returnObj[inputElems[i].name] = inputElems[i].value / 100;
+				} else {
+					returnObj[inputElems[i].name] = inputElems[i].value;
+				}
+			} else {
+				returnObj[inputElems[i].name] = null;
+			}
+		}
+	}
+	var notEditableElemsWithValue = $('.with-data', objContext);
+	for (var i = 0; i < notEditableElemsWithValue.length; i++) {
+		if (notEditableElemsWithValue[i].innerHTML) {
+			returnObj[notEditableElemsWithValue[i].id] = notEditableElemsWithValue[i].innerHTML;
+		} else {
+			returnObj[notEditableElemsWithValue[i].id] = null;
+		}
+	}
+	return returnObj;
+};
